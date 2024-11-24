4c0f092f35ff9dc9d97c0d1ad7756353fb39d480
gAmma05 <minedung2005@gmail.com>
2024-11-24 15:38:29 +0700

    add member interface

---
+++ GymMembershipManagementSystem/src/Controller/IMemberManagement.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+/**
+ *
+ * @author gAmma
+ */
+public interface IMemberManagement {
+    void buyMembership();
+    
+    void cancelMembership();
+    
+    void renewMembership();
+    
+    ////////////////////////////////////////////////
+    
+    void yourHealthMetric();
+    
+    void yourWorkoutHistory();
+}
