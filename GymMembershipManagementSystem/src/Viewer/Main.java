ac5719f80deda59417504fd49cccb4c45df47085
gAmma05 <minedung2005@gmail.com>
2024-11-24 22:06:22 +0700

    remove relunctant throw clauses

--- GymMembershipManagementSystem/src/Viewer/Main.java
+++ GymMembershipManagementSystem/src/Viewer/Main.java
@@ -7,7 +7,6 @@ package Viewer;
 
 import Access.LoginRegisterMenu;
 import ConnectToSQLServer.ConnectToSQLServer;
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
@@ -16,16 +15,22 @@ import java.sql.SQLException;
  * @author gAmma
  */
 public class Main {
-    
+
     public static LoginRegisterMenu lrm = new LoginRegisterMenu();
 
-    public static void main(String[] args) throws ClassNotFoundException, SQLException, NoSuchAlgorithmException {
-        Connection con = ConnectToSQLServer.getConnection();
-        if(con != null){
-            System.out.println("Connected to database successfully");
-            lrm.Menu();
-        }else{
-            System.out.println("Failed to connect to database");
+    public static void main(String[] args) {
+        try (Connection con = ConnectToSQLServer.getConnection()) {
+            if (con != null) {
+                System.out.println("Connected to database successfully");
+                lrm.Menu();
+            } else {
+                System.out.println("Failed to connect to database");
+            }
+        } catch (ClassNotFoundException classE) {
+            System.out.println("Class not found: " + classE.getMessage());
+        } catch (SQLException sql) {
+            System.out.println("SQL Error: " + sql.getMessage());
         }
+
     }
 }
