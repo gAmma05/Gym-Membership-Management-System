e3e028fc47654575217fce860abe6af5ab24a759
gAmma05 <minedung2005@gmail.com>
2024-11-24 21:27:58 +0700

    fix Member/Trainer class

--- GymMembershipManagementSystem/src/model/Trainer.java
+++ GymMembershipManagementSystem/src/model/Trainer.java
@@ -5,6 +5,7 @@
  */
 package model;
 
+import java.time.LocalDate;
 
 /**
  *
@@ -12,16 +13,32 @@ package model;
  */
 public class Trainer extends User {
 
+    private LocalDate joinDate;
+    private int expYear;
     private String availableSessions; // Corresponds to 'availableSessions' column
 
     public Trainer() {
     }
 
-    public Trainer(int id, String username, String password, String salt,
-             String name, String email, String role, String phoneNumber, String gender,
+    public Trainer(int id, String username, String password, String name, 
+            String email, String role, String phoneNumber, String gender, LocalDate joinDate, int expYear,
             String availableSessions) {
         super(id, username, password, name, email, role, phoneNumber, gender);
+        this.joinDate = joinDate;
+        this.expYear = expYear;
         this.availableSessions = availableSessions;
+    }
+
+    public LocalDate getJoinDate() {
+        return joinDate;
+    }
+
+    public int getExpYear() {
+        return expYear;
+    }
+
+    public void setExpYear(int expYear) {
+        this.expYear = expYear;
     }
 
     public String getAvailableSessions() {
@@ -34,7 +51,7 @@ public class Trainer extends User {
 
     @Override
     public String toString() {
-        return "Trainer{" + "availableSessions=" + availableSessions + '}';
+        return "Trainer{" + "joinDate=" + joinDate + ", " + "availableSessions=" + "expYear=" + expYear + ", "  + availableSessions + '}';
     }
 
 }
